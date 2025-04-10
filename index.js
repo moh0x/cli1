63dc683b8ee0920133fe11183b2be577bd5cf327
moh0x <141264101+moh0x@users.noreply.github.com>
2025-04-10 09:55:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ app.use(cors())
 const db = require('./db/db')
 const userRoute = require('./routes/auth_user')
 app.use('/api/user/',userRoute)
+const chanterRoute = require('./routes/chanter_route')
+app.use('/api/chanter/',chanterRoute)
 app.listen(port,async()=>{
     console.log('connected');  
     await db.connectDb()
 })
 require('dotenv').config();
-
 
 
