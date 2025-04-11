b2610723b6d989ad1685d0c096f34950cb94a748
moh0x <141264101+moh0x@users.noreply.github.com>
2025-04-11 09:07:44 +0000

    Update chanter_route.js

--- routes/chanter_route.js
+++ routes/chanter_route.js
@@ -4,6 +4,6 @@ const router = express.Router()
 const chanterController = require('../controller/chanter_controller')
 const {verifyToken} = require('../utility/verifyToken')
 router.post('/add-chanter',body("reference").isString().isLength({min:5,max:15}).withMessage("type valid reference"),body('details').isLength({max:50}).withMessage('type valid details'),body('type').isString().isLength({min:3,max:3}).withMessage('type valid type'),body("addrese").isString().isLength({min:10,max:30}).withMessage("type valid addrese"),chanterController.addChanter)
-router.get('/chanter-info',verifyToken,chanterController.chanterInfo)
+router.get('/chanter-info/:id',verifyToken,chanterController.chanterInfo)
 router.get('/chanties',verifyToken,chanterController.chanties)
 module.exports = router
