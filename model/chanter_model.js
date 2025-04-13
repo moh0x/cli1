924b9d2413cc49d3fc64812e32dda6841955f573
moh0x <141264101+moh0x@users.noreply.github.com>
2025-04-13 10:01:59 +0000

    Update chanter_model.js

--- model/chanter_model.js
+++ model/chanter_model.js
@@ -39,6 +39,28 @@ const chanterSchema = new mongoose.Schema(
             ]
         }
     },
+        sacsTwo:{
+            type:Object,
+            default:[
+                {"text":"Mélangés","desc":"Déchets mélangés hors déchets alimentaires matières dangereusesmembranes bitumineuses oU synthétiques","count":0},
+                {"text":"Plåtre","desc":"Carreaux de plåtre,placoplâtre, BA13,...","count":0},              
+                {"text":"Bois bâtiment","desc":"Charpentes, parquet,menuiseries, plinthes","count":0},
+                {"text":"Ferraille","desc":"Ferraille, acier, fonte,sac","count":0},
+                {"text":"Végétaux","desc":"Feuilles, branche","count":0},
+                {"text":"Bois autre","desc":"Meubles, palettes,mélaminé, bois decoffrage, mélange deplusieurs types de bois(bâtiment ou non)","count":0},
+                {"text":"Emballages","desc":"Carton, plastique,polystyrène,","count":0},
+                {"text":"Tuiles / Ardoises","desc":"ManutentionnableTuiles non casséesardoises, briques,parpaings,","count":0},
+            ]
+        },
+        sacsThree:{
+            type:Object,
+            default:[
+                {"text":"Encombrantslourds ou volumineux","desc":"Baignoire, armoire, ..(entre 25 et 50kg maximum)","count":0},
+                {"text":"Encombrants","desc":"WC, bac à douche, hotte,bidet, porte, ... (pas plus de 25kg)","count":0},              
+            ]
+        },
+
+    },
     { timestamps: true }
 );
 
