1f54904b8839d5e7beddfe057763cf3efb88f92e
moh0x <141264101+moh0x@users.noreply.github.com>
2025-04-13 08:44:28 +0000

    Update chanter_model.js

--- model/chanter_model.js
+++ model/chanter_model.js
@@ -23,7 +23,21 @@ const chanterSchema = new mongoose.Schema(
 			type:String,
 			required:true,
 			enum:['oui','non']
-		}
+		},
+	    sacsOne:{
+            type:Object,
+            default:[
+                {"text":"Gravets","desc":"Beton,gravier,tuile,ceramique,brique,ciment...","count":0},
+                {"text":"Terre","desc":"","count":0},
+                {"text":"Mélanges","desc":"Déchets mélangés hors déchets alimentaires matières dangereuses membranes bitumineuses ou synthétiques","count":0},
+                {"text":"Plåtre","desc":"Carreaux de plåtre,placoplâtre, BA13,...","count":0},
+                {"text":"Bois bâtiment","desc":"Charpentes, parquet,menuiseries, plinthes","count":0},
+                {"text":"Bois autre","desc":"Meubles, palettes,mélaminé, bois decoffrage, mélange deplusieurs types de bois(bâtiment ou non)","count":0},
+                {"text":"Ferraille","desc":"Ferraille, acier, fonte,sac","count":0},
+                {"text":"Végétaux","desc":"Feuilles, branche","count":0},
+
+            ]
+        }
     },
     { timestamps: true }
 );
